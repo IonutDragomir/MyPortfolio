d4be0a0b54e8f1e5e70d213a94e5bd2f1e89f42a
Ionut Marian <ionutdragomir1410@yahoo.com>
2022-08-30 14:58:38 +0300

    fixed spelling issue at work page

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
   return (
     <>
       <h2 className="head-text">
-        My creative <span>Portflio</span> section
+        My creative <span>Portfolio</span> section
       </h2>
 
       <div className="app__work-filter">
