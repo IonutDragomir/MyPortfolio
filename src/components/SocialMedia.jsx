48a96eb79cfc70e252480e796d7b8709a849b351
Ionut Marian <ionutdragomir1410@yahoo.com>
2022-08-30 14:34:47 +0300

    fixed errors and deleted unused useeffect

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,19 +4,31 @@ import { FaFacebookF, FaLinkedin, FaGithub } from "react-icons/fa";
 export default function SocialMedia() {
   return (
     <div className="app__social">
-      <a href="https://www.linkedin.com/in/ionut-dragomir-8a4b361b9/" target="_blank" >
+      <a
+        href="https://www.linkedin.com/in/ionut-dragomir-8a4b361b9/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <div>
           <FaLinkedin />
         </div>
       </a>
 
-      <a href="https://github.com/IonutDragomir" target="_blank">
+      <a
+        href="https://github.com/IonutDragomir"
+        target="_blank"
+        rel="noreferrer"
+      >
         <div>
           <FaGithub />
         </div>
       </a>
 
-      <a href="https://www.facebook.com/ionutmarian.dragomir" target="_blank">
+      <a
+        href="https://www.facebook.com/ionutmarian.dragomir"
+        target="_blank"
+        rel="noreferrer"
+      >
         <div>
           <FaFacebookF />
         </div>
