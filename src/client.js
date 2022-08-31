c5e2c5c9d00b9c455d4b5e7151837d06755b23ec
Ionut Marian <ionutdragomir1410@yahoo.com>
2022-08-31 14:01:36 +0300

    setting to private fields in client config

--- src/client.js
+++ src/client.js
@@ -2,12 +2,11 @@ import sanityClient from "@sanity/client";
 import imageURLBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "ls813ku9",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token:
-    "skNkGmybYlXldXMrZPLZHa1Q0EC5RLE1wx6nwPAG9JdrI82QBZkqEqX1FFwDP0wm8nCU8BiAPcwJUWOl1Sux42qphO4y8YpoYm0UxBpwPeWn6lvyCLq60wdWQR7jbgjHDMOfRJol0rZVBKWyQK3RS5SKlnWhYFKsRwUq6tsvROgyUAlv9CPE",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageURLBuilder(client);
